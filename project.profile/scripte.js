23561b940d12bbe20fef90adc5f72825d357d432
mohamadsam9667 <146078669+mohamadsam9667@users.noreply.github.com>
2024-02-24 15:00:16 +0330

    Add files via upload

---
+++ project.profile/scripte.js
@@ -0,0 +1,8 @@
+var typed=new Typed(".multiple-text",{
+    strings:['توسعه دهنده در زمینه فرانت اند هستم','تولید کننده محتوا','سئو'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+
+});
